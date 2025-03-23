5a66019d0ecc18fd39cd34f7afce02bc6264c396
grawnya@gmail.com <grawnya@gmail.com>
2025-03-23 11:27:00 +0000

    Created seed data js file and configured connect.js to implement seed method to show user data in console

---
+++ src/models/mongo/seed-data.js
@@ -0,0 +1,24 @@
+export const seedData = {
+    users: {
+      _model: "User",
+      gráinne: {
+        firstName: "Gráinne",
+        lastName: "O' Connor",
+        email: "grawnya@gmail.com",
+        password: "secret"
+      },
+      joan: {
+        firstName: "Joan",
+        lastName: "Russell",
+        email: "joan@yahoo.com",
+        password: "secret"
+      },
+      bart: {
+        firstName: "Thomas",
+        lastName: "Lawrance",
+        email: "tomh@outlook.com",
+        password: "secret"
+      }
+    },
+  };
+  
